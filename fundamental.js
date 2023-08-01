27dd04ba7f66cf9b46e604d00a07519e461002af
Munshi91 <mimunshi91@gmail.com>
2023-08-01 07:55:07 +0600

    test

--- fundamental.js
+++ fundamental.js
@@ -1,6 +1,21 @@
-var x = 20;
-var y = 30;
-var total = x + y;
+// var x = 20;
+// var y = 30;
+// var total = x + y;
 
-// console.log(total);
-document.write(total);
+// var price = 30;
+// var age = 35;
+
+// var parson = "jamal";
+
+// console.log(parson);
+// console.log(age);
+//  document.write(total);
+var madeicheTaka = 1000;
+
+var sobji = 400;
+var mach = 300;
+var morgi = 500;
+
+var total = sobji + mach + morgi;
+console.log(total);
+console.log(madeicheTaka - total);
