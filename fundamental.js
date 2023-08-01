b989f173d92a85904f171895e35bf33c71825e33
Munshi91 <mimunshi91@gmail.com>
2023-08-01 08:11:19 +0600

    test fixed

--- fundamental.js
+++ fundamental.js
@@ -10,12 +10,23 @@
 // console.log(parson);
 // console.log(age);
 //  document.write(total);
-var madeicheTaka = 1000;
+// var madeicheTaka = 1000;
 
-var sobji = 400;
-var mach = 300;
-var morgi = 500;
+// var sobji = 400;
+// var mach = 300;
+// var morgi = 500;
 
-var total = sobji + mach + morgi;
-console.log(total);
-console.log(madeicheTaka - total);
+// var total = sobji + mach + morgi;
+// console.log(total);
+// console.log(madeicheTaka - total);
+
+// var name = "kim";
+// var name2 = "jon";
+
+// var fullName = name + " " + name2;
+// console.log(typeof fullName);
+
+var num = 0.1;
+var num2 = 0.2;
+var sum = num + num2;
+console.log(sum);
