5b75b949cef6b9916448b5599ac1fc5fe1480321
Munshi91 <mimunshi91@gmail.com>
2023-08-05 11:28:13 +0600

    array indexOf

--- array.js
+++ array.js
@@ -1,13 +1,16 @@
-var arrayFind = [12, 14, 18, 30, 43];
-// console.log(arrayFind[3]);
+// var arrayFind = [12, 14, 18, 30, 43];
+// // console.log(arrayFind[3]);
 
-const shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
-// console.log(shopping.length);
-// console.log(shopping[0]);
-shopping[2] = 'tahini';
-// console.log(shopping);
+// const shopping = ['bread', 'milk', 'cheese', 'hummus', 'noodles'];
+// // console.log(shopping.length);
+// // console.log(shopping[0]);
+// shopping[2] = 'tahini';
+// // console.log(shopping);
 
-const random = ['tree', 795, [0, 1, 2]];
-random[2][2];
-// console.log(random);
-prectice;
+// const random = ['tree', 795, [0, 1, 2]];
+// random[2][2];
+// // console.log(random);
+
+var birds = ['Parrot', 'Falcon', 'Owl'];
+
+console.log(birds.indexOf('Owl'));
