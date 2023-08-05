5023403fb5335e2ef8f476f8b48609b12c2c1dc7
Munshi91 <mimunshi91@gmail.com>
2023-08-05 22:26:09 +0600

    if , else test

--- comparisons.js
+++ comparisons.js
@@ -2,11 +2,17 @@
 // console.log(5 != 6);
 // console.log(5 <= 6);
 // console.log(5 >= 6);
-var num1 = 56;
-var num2 = 46;
-console.log(num1 <= num2);
-
+// var num1 = 56;
+// var num2 = 46;
+// console.log(num1 <= num2);
 
 // multiple condition
 
+var iphonePrice = 50000;
+var myBudget = 100000;
 
+if (iphonePrice > myBudget) {
+  console.log("parches i phone");
+} else {
+  console.log("normal phone parches");
+}
