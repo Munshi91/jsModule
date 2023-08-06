fbeb059d509e5e77a757d3b8d45046972e1e2286
Munshi91 <mimunshi91@gmail.com>
2023-08-06 06:16:11 +0600

    Update loop.js

--- loop.js
+++ loop.js
@@ -1,8 +1,18 @@
 // revision
-var tableData = ["bottle", "pen", "mouse", "sunglass", "paper"];
-var item = tableData[3];
+// var tableData = ["bottle", "pen", "mouse", "sunglass", "paper"];
+// var item = tableData[3];
 
-tableData[2] = "keyboard";
+// tableData[2] = "keyboard";
 
-var paperIndex = tableData.indexOf("paper");
-console.log(paperIndex);
+// var paperIndex = tableData.indexOf("paper");
+// console.log(paperIndex);
+
+var roastGiven = 0;
+
+while (roastGiven < 7) {
+  console.log("Rost den, please");
+  // roastGiven= roastGiven +1;
+  // roastGiven += 1;
+  console.log(roastGiven);
+  roastGiven++;
+}
