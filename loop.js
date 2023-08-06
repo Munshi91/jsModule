d8075b8d82821626e2073b1b26fdee86c8ff6746
Munshi91 <mimunshi91@gmail.com>
2023-08-06 06:47:53 +0600

    Update loop.js

--- loop.js
+++ loop.js
@@ -24,16 +24,31 @@
 
 // Event numbers
 
-var number = 0;
-while (number <= 20) {
-  console.log(number);
-  // number++;
-  number = number + 2;
-}
-// odd numbers
+// var number = 0;
+// while (number <= 20) {
+//   console.log(number);
+//   // number++;
+//   number = number + 2;
+// }
+// // odd numbers
 
-var number = 1;
-while (number <= 10) {
-  console.log(number);
-  number = number + 2;
-}
+// var number = 1;
+// while (number <= 10) {
+//   console.log(number);
+//   number = number + 2;
+// }
+
+// // for loop
+// var roastGiven = 0;
+// while (roastGiven < 7) {
+//   console.log("Roast Den Please!");
+//   roastGiven++;
+// }
+
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//   console.log("Roast Den");
+// }
+
+// for (var i = 1; i <= 20; i += 2) {
+//   console.log(i);
+// }
