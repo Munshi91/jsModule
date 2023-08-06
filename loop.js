7d36676ff5d0ebde235f3b904308f276607e9574
Munshi91 <mimunshi91@gmail.com>
2023-08-06 06:04:18 +0600

    Update loop.js

---
+++ loop.js
@@ -0,0 +1,8 @@
+// revision
+var tableData = ["bottle", "pen", "mouse", "sunglass", "paper"];
+var item = tableData[3];
+
+tableData[2] = "keyboard";
+
+var paperIndex = tableData.indexOf("paper");
+console.log(paperIndex);
