d30e5ffc4a0dec4fdfd1d8e894c3b6dfcc83f819
Munshi91 <mimunshi91@gmail.com>
2023-08-09 06:54:06 +0600

    cube number

--- functions.js
+++ functions.js
@@ -18,14 +18,39 @@
 /* if the number is divisible by 5 then instead of the number show 'bar' */
 /* if the number is divisible by both 3 and 5  then instead of the number show 'foobar' */
 
-for (let i = 1; i <= 50; i++) {
-  if (i % 3 === 0) {
-    console.log('foo');
-  } else if (i % 5 === 0) {
-    console.log('bar');
-  } else if (i % 3 === 0 && i % 5 === 0) {
-    console.log('foobar');
-  } else {
-    console.log(i);
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 === 0) {
+//     console.log('foo');
+//   } else if (i % 5 === 0) {
+//     console.log('bar');
+//   } else if (i % 3 === 0 && i % 5 === 0) {
+//     console.log('foobar');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// function funcName() {
+//   console.log("hello Functions");
+// }
+// funcName();
+// function sumMachine(a, b) {
+//   let sum = a + b;
+//   return sum;
+// }
+// console.log(sumMachine(2, 6));
+// console.log(sumMachine(27, 636));
+
+/* javascript Problem Solve 1 */
+
+function cubeNumber(numbers) {
+  let number = 0;
+
+  for (let i = 1; i <= numbers; i++) {
+    number = i ** 3;
   }
+  return number;
 }
+
+console.log(cubeNumber(3));
+console.log(cubeNumber(4));
